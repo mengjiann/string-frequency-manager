0224c9b4dab437b152fdce86e3e0e76ead35c1b1
leemj <leemj@sph.com.sg>
2018-10-03 19:38:13 +0800

    Bare project

---
+++ src/main/java/com/mj/string_frequency_manager/StringFrequencyManagerApplication.java
@@ -0,0 +1,12 @@
+package com.mj.string_frequency_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringFrequencyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringFrequencyManagerApplication.class, args);
+	}
+}
